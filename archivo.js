f1c3959642dfb91cbd66d01473589225d12e2e5c
EnzoMarinich <marinichenzo@gmail.com>
2022-09-09 20:41:10 -0300

    arreglos y objetos

--- archivo.js
+++ archivo.js
@@ -1,35 +1,48 @@
+class stock {
+    constructor(corte, precio){
+        this.corte =  corte
+        this.precio = precio
+    }
+}
 
 
+const vacio = new stock("vacio", 1000)
+const costilla = new stock("costilla", 1100)
+const costeleta = new stock("costeleta", 800)
+
+
+const arrayStock = [vacio, costilla, costeleta]
 
 compra = prompt(`que quieres comprar? \n 1-vacio $1000 \n 2-costilla $1100 \n 3-costeleta $800 \n 4-abandonar \n 5-ver carrito`);
 
-let carrito = " "
+let carrito = []
 let total = Number(0)
+let enzo = true
 
 
 
-function vender() { while(compra != "4"){
+function vender() { while(compra != "4" && enzo == true){
     switch(compra){
         case "1":
             console.log("a seleccionado vacio")
-            carrito += "vacio \n"
-            total = total + 1000
+            carrito.push(vacio.corte)
+            total = total + vacio.precio
             break;
         case "2":
             console.log("a seleccionado costilla")
-            carrito += "costilla \n"
-            total = total + 1100
+            carrito.push(costilla.corte)
+            total = total + costilla.precio
             break;
         case "3":
             console.log("a seleccionado costeleta")
-            carrito += "costeleta \n"
-            total = total + 800
+            carrito.push(costeleta.corte)
+            total = total + costeleta.precio
             break;
         case "5":
             let pagar = prompt(`${carrito} \n total a pagar : $${total} \n 1-confirmar compra \n 2-seguir comprando`)
             if(pagar == 1){
                 alert(`a comprado: \n${carrito} a $${total}`)
-                
+                enzo = false ;
             } else if(pagar == 2){
                 break;
             } else{
@@ -41,7 +54,7 @@ function vender() { while(compra != "4"){
             console.log("no tenemos esa opcion")
     }
 
-compra = prompt(`que quieres comprar? \n 1-vacio $1000 \n 2-costilla $1100 \n 3-costeleta $800 \n 4-abandonar \n 5-ver carrito`);
+if (enzo == true){compra = prompt(`que quieres comprar? \n 1-vacio $1000 \n 2-costilla $1100 \n 3-costeleta $800 \n 4-abandonar \n 5-ver carrito`);}
 
     }
 }
